39c5a7daea3c5ffdf138a23e5cbd23bb657fd47f
Anushika/06 <anushika.24bcs10344@sst.scaler.com>
2025-10-22 00:24:48 +0530

    commit changes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,7 @@
 const express=require("express");
-const userModel = require("./models/user");
 const app=express();
 const dotenv=require("dotenv");
 const connectDB = require("./config/db");
-const passport = require('passport');
-const session = require('express-session');
-require('./config/passport');
 
 dotenv.config()
 
@@ -15,16 +11,10 @@ connectDB(process.env.dbURL)
 app.use(express.json());
 
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
+app.get('/', (req,res)=>{
+    res.json({message:"Hello from server"});
+})
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/api/auth', require('./routes/auth'));
 
 
 app.listen(3000, ()=>{
