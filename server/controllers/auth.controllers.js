d2471cd824aab9d09994b84a26b1e0648b37b32e
Anushika/06 <anushika.24bcs10344@sst.scaler.com>
2025-10-25 00:17:48 +0530

    .

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -63,6 +63,7 @@ const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       sameSite: "none", 
+      secure: true, 
       maxAge: 30 * 24 * 60 * 60 * 1000, 
     });
     
@@ -99,6 +100,7 @@ const signIn = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       sameSite: "none", 
+      secure: true, 
       maxAge: 30 * 24 * 60 * 60 * 1000, 
     });
     
@@ -119,6 +121,7 @@ const signOut = (req, res) => {
     res.cookie("token", "", {
       httpOnly: true,
       sameSite: "none", 
+      secure: true, 
       expires: new Date(0), 
     });
     res.status(200).json({ message: "Logged out successfully" });
@@ -126,8 +129,6 @@ const signOut = (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
-
-
 
 
 module.exports = { signUp, signIn, signOut };
