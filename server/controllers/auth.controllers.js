4de79027e9cbe3d2a2437f7bbe934dca0fec9ff0
Anushika/06 <anushika.24bcs10344@sst.scaler.com>
2025-10-24 23:38:08 +0530

    auth file change

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -62,7 +62,7 @@ const signUp = async (req, res) => {
     
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax", 
+      sameSite: "none", 
       maxAge: 30 * 24 * 60 * 60 * 1000, 
     });
     
@@ -98,7 +98,7 @@ const signIn = async (req, res) => {
     
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax", 
+      sameSite: "none", 
       maxAge: 30 * 24 * 60 * 60 * 1000, 
     });
     
@@ -118,7 +118,7 @@ const signOut = (req, res) => {
   try {
     res.cookie("token", "", {
       httpOnly: true,
-      sameSite: "lax", 
+      sameSite: "none", 
       expires: new Date(0), 
     });
     res.status(200).json({ message: "Logged out successfully" });
